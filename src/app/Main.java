8729670e48cbcdb4e187dcfea2b02581746b206c
notato8 <ethanshaw104@gmail.com>
2021-04-07 22:22:35 -0700

    deleted

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,8 +3,6 @@ import app.database.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("yo testing this random :");
         app.database.Database.databaseconnect();
     }
     
