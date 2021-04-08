c5e52c96deae484abe01d13a0d5a20ffddf64df1
IamUncatchable <mladam15@asu.edu>
2021-04-07 21:17:40 -0700

    database update

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,11 @@
 package app;
+import app.database.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("yo testing this random :");
+        app.database.Database.databaseconnect();
     }
+    
 }
