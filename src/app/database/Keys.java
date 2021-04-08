35a64b55be017d453bc3f1527bdc4eed2f1c5108
danlikestocode <dan37aguilera+1@gmail.com>
2021-04-07 21:54:56 -0700

    random

--- src/app/database/Keys.java
+++
@@ -1,7 +0,0 @@
-package app.database;
-
-public class Keys {
-    public static String address = "75.167.9.74:10007";
-    public static String username = "postgres";
-    public static String password = "qwerty";
-}
