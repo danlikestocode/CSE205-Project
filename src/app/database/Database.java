cd732323c6d68911ef0667f3ff68479ac4e88241
danlikestocode <dan37aguilera+1@gmail.com>
2021-04-07 21:03:07 -0700

    database

--- src/app/database/Database.java
+++ src/app/database/Database.java
@@ -1,2 +1,21 @@
-package app.database;public class Database {
+package app.database;
+import java.sql.*;
+public class Database {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Connection c = null;
+        Statement s = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection(
+                    "jdbc:postgresql://75.167.9.74:10007/usersdb", "postgres", "qwerty");
+            System.out.println("Connected to the database");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
 }
