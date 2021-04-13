669fdd526dc7aeac2218935e602f4ab76bb7fd6c
IamUncatchable <80870683+IamUncatchable@users.noreply.github.com>
2021-04-12 19:41:36 -0700

    database commands

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,10 @@ import app.database.*;
 public class Main {
     public static void main(String[] args) {
         app.database.Database.databaseconnect();
+        int a = app.database.Database.getInt("test.test","test");
+        System.out.println(a);
+        String b = app.database.Database.getString("test.test","test2");
+        System.out.println(b);
     }
     
 }
--- src/app/database/Database.java
+++ src/app/database/Database.java
@@ -1,21 +1,58 @@
 package app.database;
+
 import java.sql.*;
+
 public class Database {
+	static Statement s;
+	static Connection c;
 
-    public static void databaseconnect() {
-        // TODO Auto-generated method stub
-        Connection c = null;
-        Statement s = null;
+	public static void databaseconnect() {
+		// TODO Auto-generated method stub
 
-        try {
-            Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection(
-                    "jdbc:postgresql://" + Keys.address + "/usersdb", Keys.username, Keys.password);
-            System.out.println("Connected to the database");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-    }
+		try {
+			c = DriverManager.getConnection("jdbc:postgresql://" + Keys.address + "/usersdb", Keys.username,
+					Keys.password);
+
+			s = c.createStatement();
+			System.out.println("Connected to the database");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+	}
+
+	public static int getInt(String dataTable, String ColumnName) {
+		int result = 0;
+
+			try {
+				ResultSet rs = s.executeQuery("select * from " + dataTable + ";" );
+				rs.next();
+				result = rs.getInt(ColumnName);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+				
+
+		return result;
+	}
+	
+	public static String getString(String dataTable, String ColumnName) {
+		String result = "";
+
+			try {
+				ResultSet rs = s.executeQuery("select * from " + dataTable + ";" );
+				rs.next();
+				result = rs.getString(ColumnName);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+				
+
+		return result;
+	}
+
+
 }
