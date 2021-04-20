c6a3ec2621fc7a92cbeaf89a812e193a7c6eeb54
IamUncatchable <80870683+IamUncatchable@users.noreply.github.com>
2021-04-19 22:46:05 -0700

    search int

--- src/app/database/Database.java
+++ src/app/database/Database.java
@@ -2,13 +2,16 @@ package app.database;
 
 import java.sql.*;
 
+//This class is our database controller it contains the methods that read, write and search the database.
 public class Database {
+	//Basic global variables that are used at different instants 
 	static Statement s;
 	static Connection c;
 	static ResultSet rs;
 
+	//This method connects the database as connection c and statement s which will be
+	//used throughout the database controller
 	public static void databaseconnect() {
-		// TODO Auto-generated method stub
 
 		try {
 			c = DriverManager.getConnection("jdbc:postgresql://" + Keys.address + "/usersdb", Keys.username,
@@ -23,12 +26,17 @@ public class Database {
 		}
 	}
 
+	//basic retrieval method to get an integer from the very first row of the selected 
+	//data table in the selected column
 	public static int getInt(String dataTable, String ColumnName) {
 		int result = 0;
 
 			try {
+				//sets rs to search the data table to be read
 				rs = s.executeQuery("select * from " + dataTable + ";" );
+				//moves the query to the first line of the table
 				rs.next();
+				//gets the integer value of the first row of the selected column
 				result = rs.getInt(ColumnName);
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
@@ -39,6 +47,7 @@ public class Database {
 		return result;
 	}
 	
+	//Same as the above method but for a string
 	public static String getString(String dataTable, String ColumnName) {
 		String result = "";
 
@@ -57,15 +66,18 @@ public class Database {
 		
 	}
 	
+	//searches the selected database for a specified string in a specified column and database
 	public static void searchForString(String dataTable, String columnName, String searchItem) {
 		boolean match = false;
 		String result = "";
 		try {
+			//select the table to be searched
 			rs = s.executeQuery("Select * FROM " + dataTable + ";");
+			//search the column for the matching string
 			while(!match) {
 				rs.next();
 				result = rs.getString(columnName);
-				
+				//test result to see if it is a match
 				if (result.equals(searchItem)) {
 					match = true;
 				}
@@ -77,10 +89,31 @@ public class Database {
 		}
 	}
 	
-	public static void searchForInt(int searchItem) {
-		
+	//searches the selected database for a specified int in a specified column and database
+	public static void searchForInt(String dataTable, String columnName, int searchItem) {
+		boolean match = false;
+		int result;
+		try {
+			//select the table to be searched
+			rs = s.executeQuery("Select * FROM " + dataTable + ";");
+			//search the column for the matching int
+			while(!match) {
+				rs.next();
+				result = rs.getInt(columnName);
+				//test result to see if it is a match
+				if (result == searchItem) {
+					match = true;
+				}
+				
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
+	//returns a string from the input column from the currently selected row of the
+	//currently selected data table
 	public static String returnCurrentString(String columnName) {
 		String result = "";
 		try {
