d176176155e3ce39893e8794d0e2550ec58eaf02
notato8 <ethanshaw104@gmail.com>
2021-04-21 18:07:40 -0700

    gui structure

--- src/app/gui/LoginWindow.java
+++ src/app/gui/LoginWindow.java
@@ -5,8 +5,6 @@ import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 
 public class LoginWindow extends Window {
     ChoiceHandler choiceHandler = new ChoiceHandler();
