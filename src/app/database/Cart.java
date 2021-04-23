45fdc9303ce13857d3f3b83f3e34846c2c93857f
notato8 <ethanshaw104@gmail.com>
2021-04-22 18:44:25 -0700

    added constructor to Cart based on 2D array

--- src/app/database/Cart.java
+++ src/app/database/Cart.java
@@ -1,9 +1,30 @@
 package app.database;
 
 public class Cart {
-    private int[] ids;
+    private Product[] products;
     private int[] quantities;
 
-    public int[] getIDs() { return ids; }
+    public Product[] getIDs() { return products; }
     public int[] getQuantities() { return quantities; }
+
+    public Cart(int[][] cart2DArray) {
+
+        for (int i = 0; i < cart2DArray[0].length; i++) {
+
+            Database.searchForInt("products", "ids", cart2DArray[0][i]);
+
+            int id = Database.returnCurrentInt("ids");
+            String name = Database.returnCurrentString("names");
+            int stock = Database.returnCurrentInt("stock");
+
+            products = new Product[cart2DArray[0].length];
+            products[i] = new Product(id, name, stock);
+        }
+
+        for (int i = 0; i < cart2DArray[1].length; i++) {
+            quantities = new int[cart2DArray[1].length];
+            quantities[i] = cart2DArray[1][i];
+        }
+
+    }
 }
--- src/app/database/Product.java
+++ src/app/database/Product.java
@@ -5,6 +5,12 @@ public class Product {
     private String name;
     private int stock;
 
+    public Product(int id, String name, int stock) {
+        this.id = id;
+        this.name = name;
+        this.stock = stock;
+    }
+
     public int getID() { return id; }
     public String getName() { return name; }
     public int getStock() { return stock; }
