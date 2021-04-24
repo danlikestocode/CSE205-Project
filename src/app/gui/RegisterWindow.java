10bf157261b1776677d002dc2a688f943df2c26c
notato8 <ethanshaw104@gmail.com>
2021-04-23 20:19:01 -0700

    create manager and employee

--- src/app/gui/RegisterWindow.java
+++ src/app/gui/RegisterWindow.java
@@ -152,26 +152,23 @@ public class RegisterWindow extends Window {
         window.add(panel);
 
         //Create if else statement for desingnation
+        panel = new JPanel();
+        panel.setBackground(new Color(241, 250, 238));
+
+        label = new JLabel("Position:    ");
+        label.setFont(smallFont);
+        panel.add(label);
+
+        String position[] = {"Customer", "Employee", "Manager"};
+        positionComboBox = new JComboBox(position);
+        positionComboBox.setFont(smallFont);
+        positionComboBox.setPreferredSize(new Dimension(200,50));
+        positionComboBox.setMaximumSize(new Dimension(200, 50));
+        positionComboBox.setBorder(new LineBorder(Color.BLACK, 2));
+        panel.add(positionComboBox);
+
         if(designation == 2){
-            panel = new JPanel();
-            panel.setBackground(new Color(241, 250, 238));
-
-            label = new JLabel("Position:    ");
-            label.setFont(smallFont);
-            panel.add(label);
-
-            String position[] = {"Customer", "Employee", "Manager"};
-            positionComboBox = new JComboBox(position);
-            positionComboBox.setFont(smallFont);
-            positionComboBox.setPreferredSize(new Dimension(200,50));
-            positionComboBox.setMaximumSize(new Dimension(200, 50));
-            positionComboBox.setBorder(new LineBorder(Color.BLACK, 2));
-            panel.add(positionComboBox);
-
             window.add(panel);
-        }
-        else if (designation == 1){
-
         }
 
         //Diffrent Positions
@@ -289,10 +286,11 @@ public class RegisterWindow extends Window {
                     // Now let's check to see if their username is unique
                     if (!alreadyExists) { // the username is unique, so we will create a user
 
+
                         Database.createUser(
                             usernameTextField.getText(), new String(passwordField.getPassword()),
                             emailTextField.getText(), firstNameTextField.getText(), lastNameTextField.getText(),
-                            addressTextField.getText(), 0 //customer
+                            addressTextField.getText(), positionComboBox.getSelectedIndex() //customer
                         );
 
                         window.dispose();
